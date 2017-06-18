56f45fe1b213c40cfbefb1d46331d26618d79ac3
中島敬人 <TakahitoNakashima@taka.local>
2017-06-18 23:37:31 +0900

    ドットインストールJavascriptでおみくじを作ろう

---
+++ MyOmikuji/js/main.js
@@ -0,0 +1,35 @@
+(function() {
+    'use strict';
+
+    // 要素の取得
+    var btn = document.getElementById('btn');
+
+    // イベントリスナーの追加
+    btn.addEventListener('click', function() {
+        // 確率を変更したver
+        var n = Math.random();
+        if (n < 0.05) {
+            this.textContent = '大吉'; // 5%
+        } else if (n < 0.2) {
+            this.textContent = '中吉'; // 15%
+        } else {
+            this.textContent = '凶'; // 80%
+        }
+
+        // 通常ver
+        // var results = ['大吉', '中吉', '凶', '末吉'];
+        // // var n = Math.floor(Math.random() * 4);
+        // var n = Math.floor(Math.random() * results.length);
+        // this.textContent = results[n];
+    });
+
+    btn.addEventListener('mousedown', function() {
+        this.className = 'pushed';
+    });
+
+    btn.addEventListener('mouseup', function() {
+        this.className = '';
+    });
+
+
+}) ();
