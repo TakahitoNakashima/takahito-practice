a068b608c9cda83ee36df467ed91711555325c51
中島敬人 <TakahitoNakashima@taka.local>
2017-09-18 16:13:23 +0900

    make archetype

---
+++ gswm-web-prototype/target/generated-sources/archetype/src/main/resources/archetype-resources/src/main/java/AppStatusServlet.java
@@ -0,0 +1,18 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+@WebServlet("/status")
+public class AppStatusServlet extends HttpServlet {
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    printWriter writer = response.getWriter();
+    writer.println("OK");
+    response.setStatus(response.SC_OK);
+  }
+}
